f0e2d04a096570c8ffbbd81aefb9c2be93814c94
Ollie Shih <shih467864@gmail.com>
2025-03-22 22:49:55 +0800

    completed Ex1 Ex2

--- src/hw4/Ex1.java
+++ src/hw4/Ex1.java
@@ -3,7 +3,29 @@ package hw4;
 public class Ex1 {
 	/*
 	 * 有個一維陣列如下：{29, 100, 39, 41, 50, 8, 66, 77, 95, 15}
-		請寫出一隻程式能輸出此陣列所有元素的平均值與大於平均值的元素
+	 * 請寫出一隻程式能輸出此陣列所有元素的平均值與大於平均值的元素
 	 */
+	public static void main(String[] args) {
+		int[] Array1 = { 29, 100, 39, 41, 50, 8, 66, 77, 95, 15 };
+
+		// 計算陣列內所有數字總數
+		int sum = 0;
+
+		for (int i = 0; i < Array1.length; i++) {
+			sum += Array1[i];
+
+		}
+		System.out.println(sum);
+
+		double avg = sum / Array1.length;
+		System.out.println("平均值為：" + avg);
+
+		System.out.print("陣列內大於平均值的數字為：");
+		for (int i = 0; i < Array1.length; i++) {
+			if (Array1[i] > avg) {
+				System.out.print(Array1[i] + " ");
+			}
+		}
+	}
 
 }
--- src/hw4/Ex2.java
+++ src/hw4/Ex2.java
@@ -1,9 +1,25 @@
 package hw4;
 
+import java.util.Scanner;
+
 public class Ex2 {
 	/*
-	 * 請建立一個字串，經過程式執行後，輸入結果是反過來的
-		例如String s = “Hello World”，執行結果即為dlroW olleH
+	 * 請建立一個字串，經過程式執行後，輸入結果是反過來的 例如String s = “Hello World”，執行結果即為dlroW olleH
 	 */
+	public static void main(String[] args) {
 
+		// s1. 輸入文字並建立字串
+		System.out.println("請輸入一段句子：");
+		Scanner sc = new Scanner(System.in);
+		String str = sc.nextLine();
+
+		// s2. 將字串轉為字元陣列
+		char[] charArray = str.toCharArray();
+
+		// s3. 利用迴圈將陣列由索引大至索引小的元素列印出來
+		for (int i = charArray.length - 1; i >= 0; i--) {
+			System.out.print(charArray[i]);
+		}
+
+	}
 }
