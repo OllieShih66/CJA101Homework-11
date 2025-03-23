a31341d66b302c1ac682b7e568bf5408c66545ed
Ollie Shih <shih467864@gmail.com>
2025-03-23 23:11:45 +0800

    completed hw4 Ex3.

--- src/hw4/Ex3.java
+++ src/hw4/Ex3.java
@@ -2,9 +2,43 @@ package hw4;
 
 public class Ex3 {
 	/*
-	 * 有個字串陣列如下 (八大行星)：
-		{“mercury”, “venus”, “earth”, “mars”, “jupiter”, “saturn”, “uranus”, “neptune”}
-		請用程式計算出這陣列裡面共有多少個母音(a, e, i, o, u)
+	 * 有個字串陣列如下 (八大行星)： {“mercury”, “venus”, “earth”, “mars”, “jupiter”, “saturn”,
+	 * “uranus”, “neptune”} 請用程式計算出這陣列裡面共有多少個母音(a, e, i, o, u)
 	 */
+	public static void main(String[] args) {
+		String[] planet = { "mercury", "venus", "earth", "mars", "jupiter", "saturn", "uranus", "neptune" };
+		int count = 0;
+		for (int i = 0; i < planet.length; i++) {
 
+			// 處理大小寫
+			String planet2 = planet[i].toLowerCase();
+			for (int j = 0; j < planet[i].length(); j++) {
+				switch (planet2.charAt(j)) {
+				case 'a':
+					count++;
+					break;
+				case 'e':
+					count++;
+					break;
+				case 'i':
+					count++;
+					break;
+				case 'o':
+					count++;
+					break;
+				case 'u':
+					count++;
+					break;
+				}
+				//if判斷式用法
+//				char c = planet2.charAt(j);
+//				if (c == 'a'|| c == 'e'|| c == 'i'|| c == 'o'|| c == 'u') {
+//					count++;
+//				} 
+
+			}
+
+		}
+		System.out.println(count);
+	}
 }
